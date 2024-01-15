8beb99f4703c75f5075d9f48c299b5d61933d872
zulzakariarpl2023 <zulzakaria.rpl2023@gmail.com>
2024-01-15 13:14:45 +0800

    Belajar Operator dan Fungsi

---
+++ 1. Memulai JS/variabel.js
@@ -0,0 +1,23 @@
+// deklarasi awal 
+let _nama = "Zul";
+let usia = null;
+const jk = "L";
+let menikah;
+
+//cetak nilai awal
+console.log(_nama);
+console.log(usia);
+console.log(jk);
+console.log(menikah);
+
+//perubahan isi variabel
+_nama = "Zulkarnain"
+usia = 36;
+menikah = true;
+let email;
+
+//cetak hasil perubahan
+console.log(_nama);
+console.log(usia);
+console.log(jk);
+console.log(menikah);
---
+++ 2. Operator & Fungsi/aritmatika.js
@@ -0,0 +1,39 @@
+let bil1 = 10;
+let bil2 = 3;
+
+//Operator Penjumlahan
+let jumlah;
+jumlah = bil1 + bil2;
+console.log(jumlah);
+
+//Operator Pengurangan
+let kurang;
+kurang = bil1 - bil2;
+console.log(kurang);
+
+//Operator Perkalian
+let kali;
+kali = bil1 * bil2;
+console.log(kali);
+
+//Operator Pembagian
+let bagi;
+bagi = bil1 / bil2;
+console.log(bagi);
+
+//Eksponen (Pangkat)
+let pangkat;
+pangkat = bil1 ** bil2;
+console.log(pangkat);
+
+//Modulus (Sisa Hasil Bagi)
+let modulus;
+modulus = bil1 % bil2;
+console.log(modulus);
+
+//Increment | Decrement
+bil1++;
+console.log(bil1);
+bil2--;
+console.log(bil2);
+
